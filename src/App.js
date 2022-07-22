f33082a0f5cb0f80bd91cfa926ae80be20c7ea0c
Stephen Gallagher <sgallagher92@gmail.com>
2022-07-22 11:52:21 +0800

    finalise routing logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navigation from './components/Navigation';
+import Homepage from './pages/Homepage';
+import Subpage from './pages/Subpage';
 
 function App() {
+  const [subpageContent, setSubpageContent] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation setSubpageContent={setSubpageContent} />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route
+          path="/subpage"
+          element={<Subpage subpageContent={subpageContent} />}
+        />
+      </Routes>
     </div>
   );
 }
